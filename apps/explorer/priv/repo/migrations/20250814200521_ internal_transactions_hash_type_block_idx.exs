2991fed2526615e32ca1c11f6343c6ff56bf76bd
weequan <wee@hyperlegend.onmicrosoft.com>
2025-08-14 21:35:33 +0800

    1

--- apps/explorer/priv/repo/migrations/20250814200521_ internal_transactions_hash_type_block_idx.exs
+++ apps/explorer/priv/repo/migrations/20250814200521_ internal_transactions_hash_type_block_idx.exs
@@ -1,14 +1,23 @@
-def up do
-  # Composite index for internal_transactions
-  create(
-    index(
-      :internal_transactions,
-      [:transaction_hash, :type, :index, :block_number, :transaction_index],
-      name: :internal_transactions_hash_type_block_idx
-    )
-  )
-end
 
-def down do
-  drop(index(:internal_transactions, [:transaction_hash, :type, :index, :block_number, :transaction_index], name: :internal_transactions_hash_type_block_idx))
+
+
+
+
+defmodule Explorer.Repo.Migrations.AddAdvancedIndexes do
+  use Ecto.Migration
+
+  def up do
+    # Composite index for internal_transactions
+    create(
+      index(
+        :internal_transactions,
+        [:transaction_hash, :type, :index, :block_number, :transaction_index],
+        name: :internal_transactions_hash_type_block_idx
+      )
+    )
+  end
+
+  def down do
+    drop(index(:internal_transactions, [:transaction_hash, :type, :index, :block_number, :transaction_index], name: :internal_transactions_hash_type_block_idx))
+  end
 end
